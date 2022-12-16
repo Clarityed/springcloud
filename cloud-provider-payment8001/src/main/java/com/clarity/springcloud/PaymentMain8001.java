d816e31a24d3160cad6d252450556f740770fc3d
clarity <2270893459@qq.com>
2022-12-16 15:46:44 +0800

    Initial commit

---
+++ cloud-provider-payment8001/src/main/java/com/clarity/springcloud/PaymentMain8001.java
@@ -0,0 +1,20 @@
+package com.clarity.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * springboot 主启动类
+ *
+ * @author: clarity
+ * @date: 2022年12月14日 15:15
+ */
+
+@SpringBootApplication
+public class PaymentMain8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+
+}
