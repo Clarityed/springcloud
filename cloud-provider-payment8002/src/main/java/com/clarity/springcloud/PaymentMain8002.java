df0ce9b9166ec54fdfd41e993a1eb5c164e35016
clarity <2270893459@qq.com>
2022-12-18 14:58:09 +0800

    realise 支付服务提供者，实现集群部署

---
+++ cloud-provider-payment8002/src/main/java/com/clarity/springcloud/PaymentMain8002.java
@@ -0,0 +1,22 @@
+package com.clarity.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * springboot 主启动类
+ *
+ * @author: clarity
+ * @date: 2022年12月14日 15:15
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+public class PaymentMain8002 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8002.class, args);
+    }
+
+}
