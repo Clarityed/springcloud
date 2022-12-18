b788245515ca4072269a17ed3e51651de6d05388
clarity <2270893459@qq.com>
2022-12-18 15:04:34 +0800

    add 添加负载均衡

--- cloud-consumer-order80/src/main/java/com/clarity/springcloud/config/RestTemplateConfig.java
+++ cloud-consumer-order80/src/main/java/com/clarity/springcloud/config/RestTemplateConfig.java
@@ -1,5 +1,6 @@
 package com.clarity.springcloud.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -15,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
 
     @Bean
+    @LoadBalanced // 使用 @LoadBalanced 注解赋予 RestTemplate 负载均衡能力
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
