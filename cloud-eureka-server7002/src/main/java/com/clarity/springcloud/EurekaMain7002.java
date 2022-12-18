26d018d017afdc6a30f653e96c5d9adfd803e38e
clarity <2270893459@qq.com>
2022-12-18 10:45:52 +0800

    build Eureka 集群 2 台

---
+++ cloud-eureka-server7002/src/main/java/com/clarity/springcloud/EurekaMain7002.java
@@ -0,0 +1,21 @@
+package com.clarity.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 主启动类
+ *
+ * @author: clarity
+ * @date: 2022年12月17日 11:58
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
