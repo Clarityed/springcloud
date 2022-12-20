248bb6b84895ff5b424c7ac013f66a414b49385e
clarity <2270893459@qq.com>
2022-12-20 11:53:06 +0800

    modify 服务客户端使用 get / post Entity 的方法，发生请求给服务端。

--- cloud-consumer-order80/src/main/java/com/clarity/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/clarity/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.clarity.springcloud.controller;
 import com.clarity.springcloud.entities.CommonResult;
 import com.clarity.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -33,11 +34,39 @@ public class OrderController {
         return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
     }
 
+    @GetMapping("/payment/createEntity")
+    public CommonResult<Payment> createEntity(Payment payment) {
+        if (payment == null) {
+            return new CommonResult(500, "插入记录不能为空", null);
+        }
+        ResponseEntity<CommonResult> commonResultResponseEntity = restTemplate.postForEntity(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
+        if (commonResultResponseEntity.getStatusCode().is2xxSuccessful()) {
+            log.info("请求对象信息：" + commonResultResponseEntity.getStatusCode() + commonResultResponseEntity.getHeaders());
+            return commonResultResponseEntity.getBody();
+        }
+
+        return new CommonResult<>(500, "请求返回发生错误", null);
+    }
+
     @GetMapping("/payment/get/{id}")
-    public CommonResult<Payment> create(@PathVariable Long id) {
+    public CommonResult<Payment> get(@PathVariable Long id) {
         if (id <= 0) {
             return new CommonResult(500, "该数据不可能存在", null);
         }
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class, id);
+    }
+
+    @GetMapping("/payment/getEntity/{id}")
+    public CommonResult<Payment> getEntity(@PathVariable Long id) {
+        if (id <= 0) {
+            return new CommonResult(500, "该数据不可能存在", null);
+        }
+        ResponseEntity<CommonResult> responseEntity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+            log.info("请求对象信息：" + responseEntity.getStatusCode() + responseEntity.getHeaders());
+            return responseEntity.getBody();
+        }
+
+        return new CommonResult<>(500, "请求返回发生错误", null);
     }
 }
