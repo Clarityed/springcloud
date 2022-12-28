19254b5d40b619926edaadadd275af685124dc07
clarity <2270893459@qq.com>
2022-12-28 11:20:28 +0800

    add 服务监控 hystrixDashboard

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/clarity/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,21 @@
+package com.clarity.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * 主启动
+ *
+ * @author: clarity
+ * @date: 2022年12月28日 10:37
+ */
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
