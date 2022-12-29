0eebf73f33dd020dcd7dc13fc921deea8dd02bc1
clarity <2270893459@qq.com>
2022-12-29 12:14:27 +0800

    add Gateway 网关，动态路由配置，并且实现负载均衡算法

--- cloud-gateway-gateway9527/src/main/java/com/clarity/springcloud/config/GatewayConfig.java
+++ cloud-gateway-gateway9527/src/main/java/com/clarity/springcloud/config/GatewayConfig.java
@@ -1,3 +1,4 @@
+/*
 package com.clarity.springcloud.config;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -5,22 +6,26 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+*/
 /**
  * 路由规则配置
  *
  * @author: clarity
  * @date: 2022年12月29日 11:25
- */
+ *//*
+
 
 @Configuration
 public class GatewayConfig {
 
-    /**
+    */
+/**
      * 配置了一个id为route-name的路由规则，
      * 当访问地址 http://localhost:9527/guonei时会自动转发到地址：http://news.baidu.com/guonei
      * @param routeLocatorBuilder
      * @return
-     */
+     *//*
+
     @Bean
     public RouteLocator customRouteLocator1(RouteLocatorBuilder routeLocatorBuilder) {
         RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
@@ -35,3 +40,4 @@ public class GatewayConfig {
         return routes.build();
     }
 }
+*/
