bba75cb4b5fb3cddd1c9f24a5a74b96afedd7ebf
clarity <2270893459@qq.com>
2022-12-31 17:59:45 +0800

    add Spring Cloud Config 分布式配置中心，Config 服务端配置与测试

---
+++ cloud-config-center-3344/src/main/java/com/clarity/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,21 @@
+package com.clarity.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 主启动
+ *
+ * @author: clarity
+ * @date: 2022年12月31日 15:33
+ */
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
