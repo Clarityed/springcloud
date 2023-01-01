0d049992bf4f4338d91a68835292caf78880397f
clarity <2270893459@qq.com>
2023-01-01 16:13:18 +0800

    add Spring Cloud Config 分布式配置中心，Config 客户端之动态刷新（手动版，客户端必须发送 POST 请求才能同步最新配置）

--- cloud-config-client-3355/src/main/java/com/clarity/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/com/clarity/springcloud/controller/ConfigClientController.java
@@ -2,6 +2,7 @@ package com.clarity.springcloud.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@RefreshScope
 @RequestMapping("/config")
 public class ConfigClientController {
 
