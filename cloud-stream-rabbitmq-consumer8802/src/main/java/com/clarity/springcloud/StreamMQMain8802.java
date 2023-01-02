48926c0c76e09b33f91e52a1cd9f16a27e52ca55
clarity <2270893459@qq.com>
2023-01-02 16:28:57 +0800

    add Spring Cloud stream，添加消息驱动之消费者

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/clarity/springcloud/StreamMQMain8802.java
@@ -0,0 +1,19 @@
+package com.clarity.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 主启动类
+ *
+ * @author: clarity
+ * @date: 2023年01月02日 16:12
+ */
+
+@SpringBootApplication
+public class StreamMQMain8802 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8802.class, args);
+    }
+}
