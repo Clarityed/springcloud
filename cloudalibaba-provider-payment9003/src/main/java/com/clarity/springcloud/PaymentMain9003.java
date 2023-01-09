b70bcb55952da8eec52e646de1abbecc878e8dca
clarity <2270893459@qq.com>
2023-01-09 17:51:30 +0800

    add Spring Cloud Alibaba Sentinel, 测试服务熔断功能 Ribbon 系列，准备工作，服务提供者 9003 创建

---
+++ cloudalibaba-provider-payment9003/src/main/java/com/clarity/springcloud/PaymentMain9003.java
@@ -0,0 +1,21 @@
+package com.clarity.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 主启动
+ *
+ * @author: clarity
+ * @date: 2023年01月09日 17:41
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9003 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9003.class, args);
+    }
+}
